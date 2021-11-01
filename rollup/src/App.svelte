c3916bc5ecf7b1f01feaeff7c05c0dd4fee08d73
Claude Barde <lotolosenc.oc@gmail.com>
2021-11-01 09:42:08 +0000

    Added configuration for Taquito dapp with Rollup and Svelte

---
+++ rollup/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { TezosToolkit } from "@taquito/taquito";
+
+  let Tezos: TezosToolkit;
+
+  onMount(() => {
+    Tezos = new TezosToolkit("https://mainnet.api.tez.ie");
+  });
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello Taquito!</h1>
+  <p>test</p>
+</main>
