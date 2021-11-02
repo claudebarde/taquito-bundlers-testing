dff8128fbd61e0d8707d6d1a2c4b96f66756adc3
Claude Barde <lotolosenc.oc@gmail.com>
2021-11-02 09:51:03 +0000

    Testing dapp with Beacon wallet

--- rollup/src/App.svelte
+++ rollup/src/App.svelte
@@ -1,14 +1,48 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { TezosToolkit } from "@taquito/taquito";
+  import { BeaconWallet } from "@taquito/beacon-wallet";
+  import { NetworkType } from "@airgap/beacon-sdk";
 
+  const rpcUrl = "https://mainnet.api.tez.ie";
   let Tezos: TezosToolkit;
   let currentLevel: number | null = null;
+  let wallet: BeaconWallet;
+  let userAddress = "";
+
+  const connectWallet = async () => {
+    await wallet.requestPermissions({
+      network: {
+        type: NetworkType.MAINNET,
+        rpcUrl
+      }
+    });
+    Tezos.setWalletProvider(wallet);
+    userAddress = await wallet.getPKH();
+  };
+
+  const disconnectWallet = () => {
+    wallet.client.destroy();
+    wallet = undefined;
+    userAddress = "";
+  };
 
   onMount(async () => {
-    Tezos = new TezosToolkit("https://mainnet.api.tez.ie");
+    Tezos = new TezosToolkit(rpcUrl);
     const blockHeader = await Tezos.rpc.getBlockHeader();
     currentLevel = blockHeader.level;
+    try {
+      wallet = new BeaconWallet({
+        name: "Taquito Testing with Rollup",
+        preferredNetwork: NetworkType.MAINNET
+      });
+      const activeAccount = await wallet.client.getActiveAccount();
+      if (activeAccount) {
+        userAddress = await wallet.getPKH();
+      }
+    } catch (error) {
+      console.error(error);
+    }
   });
 </script>
 
@@ -37,4 +71,11 @@
 <main>
   <h1>Hello Taquito!</h1>
   <p>Current level: {currentLevel ?? "Loading..."}</p>
+  <div>
+    {#if userAddress}
+      <button on:click={disconnectWallet}>Disconnect wallet</button>
+    {:else}
+      <button on:click={connectWallet}>Connect wallet</button>
+    {/if}
+  </div>
 </main>
